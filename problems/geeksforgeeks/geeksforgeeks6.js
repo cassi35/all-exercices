25e94911c405a449e96af52beb2af8a6457dabd1
cassi35 <cgrsobral@gmail.com>
2025-02-03 13:40:40 -0300

    fiz exercicios geeksforgeeks

--- problems/geeksforgeeks/geeksforgeeks6.js
+++ problems/geeksforgeeks/geeksforgeeks6.js
@@ -415,7 +415,197 @@ function perfectSum(arr, target) {
 }
 
 // Testando a função
-console.log(perfectSum([5, 2, 3, 10, 6, 8], 10)); // Saída: 3
+// console.log(perfectSum([5, 2, 3, 10, 6, 8], 10)); // Saída: 3
 // console.log(perfectSum([2, 5, 1, 4, 3], 10));     // Saída: 3
 // console.log(perfectSum([5, 7, 8], 3));            // Saída: 0
 // console.log(perfectSum([35, 2, 8, 22], 0));       // Saída: 1
+//Multiply two strings
+function multiply_string(s1,s2){
+    if(isNaN(s1) || isNaN(s2)){
+        return 'invalid  input'
+    }else{
+        if(s1.includes(0)){
+            let arr_str = s1 
+            let nums = ''
+            for(let i in arr_str){
+                if(arr_str[i] != 0){
+                    nums+=arr_str[i]*s2 
+                }
+            }
+            return 0 - nums
+        }else{
+            let arr_str = s1
+            let nums 
+            for(let i in arr_str){
+                if(i == 0){
+                    nums = arr_str[i] * s2 
+                }else{
+                    let add = arr_str[i] * s2 
+                    let str = String(nums).concat(String(add))
+                    nums = str * 1
+                }
+            }
+            return  nums 
+        }
+    }
+}
+function entrada_multiply_string(){
+    let s1 = '33'
+    let s2 = '2'
+    let multiply = multiply_string(s1,s2)
+    console.log(multiply)
+
+}
+// entrada_multiply_string()
+
+
+//Count Inversions
+
+function count_inverstion(arr){
+    if(arr.length == 0){
+        return null 
+    }else{
+        let count = 0
+        for(let i = 0;i < arr.length ;i++){
+            for(let j = i+1;j < arr.length;j++){
+                if(arr[i] > arr[j]){
+                    count++
+                }
+            }
+        }
+        return count
+    }
+}
+function entrada_count_inverstion(){
+    let arrs = [2, 3, 4, 5, 6]
+    let inversion = count_inverstion(arrs)
+    console.log(inversion)
+}
+// entrada_count_inverstion()
+
+
+
+//0 - 1 Knapsack Problem
+
+function maximum_sum_knapsack(capacity,val,wt){
+    let max = 0
+    let number = null
+    for(let i in val){
+        for(let j in wt){
+            let soma = wt[j] + val[i]
+            if(soma >= capacity && max < soma && wt[j] < capacity){
+                number = val[i]
+                max = soma
+            }
+        }
+    }
+    return number
+}
+function entrada_maximum_sum_knapsack(){
+    let capacity = 4
+    let val = [10, 40, 30, 50]
+    let wt = [5, 4, 6, 3]
+    let max = maximum_sum_knapsack(capacity,val,wt)    
+    console.log(max)
+}
+// entrada_maximum_sum_knapsack()
+
+
+
+//findUnion
+function findUnion(a,b){
+    let elements = []
+    let union = a.concat(...b)
+    for(let element of union){
+        if(elements.includes(element) == false && element != undefined){
+            elements.push(element)
+        }
+    }
+   function bubble_sort(arr){
+    let swaped 
+    do{
+        swaped = false
+        for(let i = 0;i < arr.length-1;i++){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swaped = true
+            }
+        }
+    }while(swaped)
+        return arr
+   }
+   return bubble_sort(elements)
+
+}
+function entrada_findUnion(){
+    let a = [2, 2, 3, 4, 5]
+    let b = [1, 1, 2, 3, 4]
+    let union = findUnion(a,b)
+    console.log(union)
+}
+// entrada_findUnion()
+//Smallest Positive Missing
+function smallest_positive(numbers){
+    let arrs = []
+    for(let num of numbers){
+        if(!arrs.includes(num) && num > 0){
+            arrs.push(num )
+        }
+    }
+    function insertion_sort(arr){
+        for(let i = 0;i < arr.length;i++){
+            let key = arr[i]
+            let j = i-1
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j]
+                j--
+            }
+            arr[j+1] = key
+        }
+        return arr
+    }
+    arrs = insertion_sort(arrs)
+    for(let i = 1;i < arrs.length;i++){
+        let diferenca = arrs[i] - arrs[i-1]
+        if(diferenca == 2){
+            return arrs[i] -1 //coisa linda
+        }
+    }
+  
+    return -1
+}
+function entrada_smallest_positive(){
+    let arr = [2,-3,1,4,1,7]
+    console.log(smallest_positive(arr))
+}
+
+entrada_smallest_positive()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
