74e13b6027b5d514617e1a62c58d258af038fae3
cassi35 <cgrsobral@gmail.com>
2025-03-11 13:56:04 -0300

    fiz teste de automocao de email

--- automation/automation.js
+++ automation/automation.js
@@ -21,4 +21,31 @@ function change(s, prog, version) {
 });
 }
 // change("Tues April 9, 2005", "Ladder", 1.1); // Chamando a função
+const nodemailer = require('nodemailer')
+const html = `
+  <h1>hello world</h1>
+  <p>isn't nodemaler useful?</p>
+`
+async function main(){
+ const transporter =  nodemailer.createTransport({
+    host:'smtp.gmail.com',
+    port:465,
+    secure:true,
+    auth:{
+      user:'automocaoemail@gmail.com',
+      pass:'hbzd cues nksp fmkf'
+    }
+  })
+  const info = await transporter.sendMail({
+    from: '"automocao de email" <automocaoemail@gmail.com>', 
+    to: 'altsobral@gmail.com', // Email do destinatário
+    subject: 'Assunto do Email', // Assunto do email
+    text: 'voce abriu uma conta banco itau', // Corpo do email em texto simples
+    html: html // Corpo do email em HTML
+  });
+  
+  console.log("message sent:"+info.messageId)
+}
+// main()
+// .catch(e => console.log(e))
 
